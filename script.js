722d21e93035cb1003814f79758746531040289e
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2021-08-11 20:22:36 +0530

    Dark Theme Added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Header Logo 
+
 const logo = document.querySelector('.logo-text')
 logo.addEventListener('mouseover', newLogoText)
 logo.addEventListener('mouseout', originalLogoText)
@@ -12,5 +14,24 @@ function originalLogoText(){
     logo.innerHTML = 'NT'
     logo.classList.remove('text-animate')
     logo.classList.add('text-out')
+}
+
+//Theme Switcher
+const switcher = document.querySelector('.theme-switch');
+
+const defaultTheme = localStorage.getItem('theme') || 'light-theme'; 
+setTheme(defaultTheme); 
+
+switcher.addEventListener('change', (e) => {
+    // console.log(e.target.value); 
+    setTheme(e.target.value); 
+});
+
+function setTheme(theme){
+    theme = theme || 'light-theme'; 
+    document.documentElement.className = theme; 
+    
+    localStorage.setItem('theme', theme); 
+    switcher.value = theme; 
 }
 
